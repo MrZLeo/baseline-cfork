c3045e8fcc174c010b12f0cf4a0de53630326d70
Zhang Jing <mrzleo@foxmail.com>
2023-04-12 05:58:52 +0000

    fix: use 'a' as flag when append data into file

--- daemon.py
+++ daemon.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     startTime = int(round(time.time() * 1000))
-    logf = open("log.txt", "w")
+    logf = open("log.txt", "a")
     logf.write("finish at: " + str(startTime))
     logf.close()
     return 'Hello python!\n'
@@ -14,7 +14,7 @@ def hello_world():
 if __name__ == '__main__':
     # time that start serverless function
     startTime = int(round(time.time() * 1000))
-    logf = open("log.txt", "w")
+    logf = open("log.txt", "a")
     logf.write(str(startTime))
     logf.close()
 
