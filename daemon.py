71249a709c72a881b136937ab02979cf33cbfbc5
Zhang Jing <mrzleo@foxmail.com>
2023-04-23 11:04:56 +0000

    delete unuse file operation

--- daemon.py
+++ daemon.py
@@ -5,17 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # startTime = int(round(time.time() * 1000))
-    # logf = open("log.txt", "a")
-    # logf.write("finish at: " + str(startTime))
-    # logf.close()
     return 'Hello python!\n'
 
 if __name__ == '__main__':
     # time that start serverless function
-    # startTime = int(round(time.time() * 1000))
-    # logf = open("log.txt", "a")
-    # logf.write(str(startTime))
-    # logf.close()
+    startTime = int(round(time.time() * 1000))
+    logf = open("log.txt", "a")
+    logf.write(str(startTime))
+    logf.close()
     from waitress import serve
     serve(app, host='0.0.0.0',port=int(8080))
